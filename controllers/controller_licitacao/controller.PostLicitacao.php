b8bd7c37b1e8904031f707777bec39b98fd5305d
dev-conderg <sergio.conderg.github@gmail.com>
2025-07-18 16:25:28 -0300

    validacao de dados do formulario

--- controllers/controller_licitacao/controller.PostLicitacao.php
+++ controllers/controller_licitacao/controller.PostLicitacao.php
@@ -12,6 +12,8 @@ try {
         $licitacao = new Licitacao();
         /**
          * Variáveis que chegham do formulario
+         * isset->verifica se a variavwel esta vazia ou nula
+         * trim->retira os espacos
          */
         $titulo = isset($_POST['titulo']) ? trim($POST('titulo')) : '';
         $data_publicacao = isset($_POST['data_publicacao']) ? trim($_POST('data_publicacao')) : '';
@@ -22,11 +24,37 @@ try {
         /**
          * validação dos dados
          */
-        
-        
+        $erros = array();
 
+        if($titulo == ''){
+            $erros[] = 'Titulo é obrigatório.';
+        }
 
+        if($data_publicacao == ''){
+            $erros[] = 'Data de publicação é obrigatória.';
+        }
+
+        if($data_edital == ''){
+            $erros[] = 'Data de edital é obrigatória.';
+        }
+
+        if($descritivo == ''){
+            $erros[] = 'Descritivo é obrigatório.';
+        }
+
+        if($arquivos_url == ''){
+            $erros[] = 'Arquivo é obrigatório.';
+        }
         
+        if(count($erros) > 0){
+            echo "Erros econtrados:<br>";
+            foreach ($erros as $erro) {
+                echo "- " . $erro . "<br>";
+            }
+        }else{
+            echo "Dados válidos!<br>";
+            // Aqui você pode salvar no banco ou continuar o processamento
+        }
     }
 } catch (\Throwable $th) {
     //throw $th;
